c5f33cba6cb10c22341475354541bfd5cb1f2fac
Dave Spina <dspina79@gmail.com>
2021-04-02 15:56:26 -0400

    Hello world and basics

---
+++ variables/basics.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go is a " + "programming language")
+
+	fmt.Println("The quoitient of 10.0 and 3.0 is", 10.0/3.0)
+	fmt.Println("What is the logical value of true and false? ", true || false)
+	var name = "Dean"
+	fmt.Println("Hello, ", name)
+
+}
