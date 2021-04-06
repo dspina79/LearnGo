8c596752ed3d507b77e7ec67abc5e4d241106d07
Dave Spina <dspina79@gmail.com>
2021-04-06 11:30:52 -0400

    New map operations

--- maps/maps.go
+++ maps/maps.go
@@ -13,5 +13,23 @@ func main() {
 	fmt.Println("Changing Dean to 201")
 	m["Dean"] = 201
 	fmt.Println(m)
+	fmt.Println("Adding new people")
+	m["Sharon"] = 301
+	m["Peggy"] = 422
+	fmt.Println(m)
+
+	fmt.Println("Removing Sharon")
+	delete(m, "Sharon")
+	fmt.Println(m)
+
+	fmt.Println("Accessing invalid entry")
+	_, something := m["something"]
+	fmt.Println("Something =", something)
+
+	if something {
+		fmt.Println("Something was found!")
+	} else {
+		fmt.Println("Something was not found! It doesn't exist in the map.")
+	}
 
 }
