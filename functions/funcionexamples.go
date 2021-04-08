b1fbdea63be2f81852b2507c628a290287c41754
Dave Spina <dspina79@gmail.com>
2021-04-07 22:01:31 -0400

    Added multiple return values

--- functions/funcionexamples.go
+++ functions/funcionexamples.go
@@ -17,6 +17,22 @@ func divide(x, y int) float32 {
 	return fx / fy
 }
 
+func getMinMax(vals []int) (int, int) {
+	min := 0
+	max := 0
+
+	for i, val := range vals {
+		if i == 0 || val < min {
+			min = val
+		}
+		if i == 0 || val > max {
+			max = val
+		}
+	}
+
+	return min, max
+}
+
 func main() {
 	var x int = 10
 	var y int = 15
@@ -24,4 +40,12 @@ func main() {
 	fmt.Println(x, " + ", y, "=", add(x, y))
 	fmt.Println(x, " x ", y, "=", multiply(x, y))
 	fmt.Println(x, " / ", y, "=", divide(x, y))
+
+	values := []int{23, 4, 234, 234, 234, 45, -2, 3452, 532, 901, 32, 12, 31}
+	fmt.Println("Array is: ", values)
+	min, max := getMinMax(values)
+
+	fmt.Println("The min of hte array is: ", min)
+	fmt.Println("The max of hte array is: ", max)
+
 }
