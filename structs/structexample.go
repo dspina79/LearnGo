2cf7fa6bd47e55e4dea227d8162017f8df4ab60f
Dave Spina <dspina79@gmail.com>
2021-04-11 21:48:51 -0400

    Added methods to struct and refactored print method

--- structs/structexample.go
+++ structs/structexample.go
@@ -8,6 +8,18 @@ type animal struct {
 	animalType string
 }
 
+func (a *animal) incrementAge() {
+	a.age += 1
+}
+
+func (a animal) getNameType() string {
+	return a.name + "\tType: " + a.animalType
+}
+
+func (a animal) toString() string {
+	return "Name: " + a.name + "\tType: " + a.animalType + "\tAge: " + fmt.Sprint(a.age)
+}
+
 func newAnimal(name string, animalType string) *animal {
 	a := animal{name: name}
 	a.age = 0
@@ -17,10 +29,25 @@ func newAnimal(name string, animalType string) *animal {
 }
 
 func printAnimal(objAnimal animal) {
-	fmt.Println("Name:", objAnimal.name, "\tType:", objAnimal.animalType, "\tAge:", objAnimal.age)
+	fmt.Println(objAnimal.toString())
 }
 
 func main() {
 	myAnimal := newAnimal("Fido", "Dog")
 	printAnimal(*myAnimal)
+
+	mySecondAnimal := animal{name: "Gerald", animalType: "Cat", age: 12}
+	myThirdAnimal := animal{"Minne", 8, "Leopard"} // follows the order of the structs
+	printAnimal(mySecondAnimal)
+	printAnimal(myThirdAnimal)
+
+	fmt.Println("Incrememnt all ages")
+	myAnimal.incrementAge()
+	mySecondAnimal.incrementAge()
+	myThirdAnimal.incrementAge()
+
+	printAnimal(*myAnimal)
+	printAnimal(mySecondAnimal)
+	printAnimal(myThirdAnimal)
+
 }
