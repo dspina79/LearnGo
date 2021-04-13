6cefffda028c29bf6be4a695dda7fa7aa89fa5d0
Dave Spina <dspina79@gmail.com>
2021-04-13 16:39:39 -0400

    Added custom error

--- errors/errorusage.go
+++ errors/errorusage.go
@@ -5,12 +5,49 @@ import (
 	"fmt"
 )
 
+type myError struct {
+	errorNum     int
+	errorMessage string
+}
+
+func (myErr *myError) Error() string {
+	return fmt.Sprintf("%d: %s", myErr.errorNum, myErr.errorMessage)
+}
+
 func divde(x, y int) (float32, error) {
 	if y == 0 {
 		return 0, errors.New("Cannot divide by zero")
 	}
 
 	return float32(x) / float32(y), nil
+}
+
+func checkPassword(pass string) (bool, error) {
+	if pass == "" {
+		// can't allow blank passwords
+		return false, &myError{errorNum: 0, errorMessage: "Password cannot be blank."}
+	}
+
+	if pass == "correctpass" {
+		return true, nil
+	} else {
+		return false, &myError{errorNum: 1, errorMessage: "Password is incorrect."}
+	}
+
+}
+
+func testPassword() {
+	var password string
+
+	fmt.Print("Enter the password: ")
+	fmt.Scanf("%s", password)
+
+	passResult, passError := checkPassword(password)
+	if passResult {
+		fmt.Println("Password is correct.")
+	} else {
+		fmt.Println(passError)
+	}
 }
 
 func main() {
@@ -29,4 +66,5 @@ func main() {
 	} else {
 		fmt.Println("The quotient is:", result)
 	}
+
 }
